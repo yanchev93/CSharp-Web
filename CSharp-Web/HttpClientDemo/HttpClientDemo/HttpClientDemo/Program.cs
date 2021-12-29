78d89773af38d9e12a8300dccceb7c156c094c41
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-12-29 13:04:22 -0500

    Exploring HttpClient class

---
+++ CSharp-Web/HttpClientDemo/HttpClientDemo/HttpClientDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string url = "https://softuni.bg/";
+            HttpClient httpClient = new HttpClient();
+
+            var html = await httpClient.GetStringAsync(url);
+
+            Console.WriteLine(html);
+        }
+    }
+}
