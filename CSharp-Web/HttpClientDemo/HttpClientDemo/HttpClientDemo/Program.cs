8331bb0ab342b5e527291121db1b32076afaa349
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-12-29 17:41:17 -0500

    Finished with the response part

--- CSharp-Web/HttpClientDemo/HttpClientDemo/HttpClientDemo/Program.cs
+++ CSharp-Web/HttpClientDemo/HttpClientDemo/HttpClientDemo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,30 @@ namespace HttpClientDemo
     {
         static async Task Main(string[] args)
         {
-            await ReadData();
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 8080);
+            tcpListener.Start();
+
+            while (true)
+            {
+                var client = tcpListener.AcceptTcpClient();
+                var stream = client.GetStream();
+
+                byte[] buffer = new byte[1_000_000];
+                int byteLength = 0;
+
+                var length = stream.Read(buffer, byteLength, buffer.Length);
+
+                var requestedString =
+                    Encoding.UTF8.GetString(buffer, byteLength, length);
+
+                Console.WriteLine(requestedString);
+
+                Console.WriteLine(new string('=', 50));
+                // stream.Write();
+                // stream.Read();
+            }
+
+
         }
 
         public static async Task ReadData()
