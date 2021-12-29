242dabb4559d7b7d65d8ded80a49956b80940ba0
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-12-29 13:17:05 -0500

    Reading response from the server

--- CSharp-Web/HttpClientDemo/HttpClientDemo/HttpClientDemo/Program.cs
+++ CSharp-Web/HttpClientDemo/HttpClientDemo/HttpClientDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,21 @@ namespace HttpClientDemo
     {
         static async Task Main(string[] args)
         {
+            await ReadData();
+        }
+
+        public static async Task ReadData()
+        {
             Console.OutputEncoding = Encoding.UTF8;
             string url = "https://softuni.bg/";
             HttpClient httpClient = new HttpClient();
+            var respones = await httpClient.GetAsync(url);
 
-            var html = await httpClient.GetStringAsync(url);
+            Console.WriteLine(respones.StatusCode);
+            Console.WriteLine(string.Join(Environment.NewLine, respones.Headers.Select(x => x.Key + ": " + x.Value)));
 
-            Console.WriteLine(html);
+            // var html = await httpClient.GetStringAsync(url);
+            // Console.WriteLine(html);
         }
     }
 }
